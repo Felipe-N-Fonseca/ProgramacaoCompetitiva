b33e8e55818b6dc03a438ddd65c66be1b260208a
Felipe-N-Fonseca <felipenasfonseca@gmail.com>
2024-10-15 18:49:58 -0300

    treino individual 15/10/2024

---
+++ Becrowd/Codigo/1002.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double r;
+  cin >> r;
+  r *= r;
+  cout << setprecision(4);
+  cout << "A=" << fixed << r * 3.14159 << endl; 
+  
+  return 0;
+}
---
+++ Becrowd/Codigo/1005.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double a, b;
+  cin >> a >> b;
+  cout << setprecision(5);
+  cout << "MEDIA = " << fixed << (a+b)/2 << endl;
+  
+  return 0;
+}
